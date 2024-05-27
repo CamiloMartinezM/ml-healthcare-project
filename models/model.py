2d65961ab4b9dcfb95c2aa4b5ec0f34b12c01ee2
Camilo Martínez M <cama00005@stud.uni-saarland.de>
2024-05-27 14:55:39 +0200

    Initial commit

---
+++ models/model.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# File: models/mymodel.py
+# Description: This file defines the MyModel class, which represents the data 
+# that the tool is working with.
+
+class MyModel:
+    def __init__(self, data):
+        self.data = data
---
+++ services/service.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# File: services/myservice.py
+# Description: This file defines the MyService class, which handles the main 
+# functionality of the tool.
+
+class MyService:
+    def get_data(self):
+        # Replace this with your actual functionality
+        return 'Hello, world!'
