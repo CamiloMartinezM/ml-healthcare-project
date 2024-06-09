ccb303b63705e5297f882271224a6226bb1c545d
CamiloMartinezM <cama00005@stud.uni-saarland.de>
2024-06-09 14:19:14 +0200

    Added remaining files

--- views/view.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# File: views/console_view.py
-# Description: This file defines the ConsoleView class, which handles 
-# outputting data to the console.
-
-class ConsoleView:
-    def display(self, data):
-        print(data)
